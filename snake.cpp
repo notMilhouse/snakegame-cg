a324fb0809a214cd870b175e64bae847b5920445
notmilhouse <gabriel_carvalho@usp.br>
2022-09-22 17:58:20 -0300

    began implementation of snake logic

---
+++ snake.cpp
@@ -0,0 +1,104 @@
+class GamePos {
+    public:
+        GamePos() {
+            posX = 15;
+            posY = 15;
+        }
+
+        GamePos(int x, int y) {
+            posX = x;
+            posY = y;
+        }
+
+        void setPos(int x, int y) {
+            posX = x;
+            posY = y;
+        }
+
+        int getX() {
+            return posX;
+        }
+
+        int getY() {
+            return posY;
+        }
+
+        void incrementX() {
+            posX++;
+        }
+
+        void incrementY() {
+            posY++;
+        }
+
+        void incrementX(int increment) {
+            posX += increment;
+        }
+
+        void incrementY(int increment) {
+            posY += increment;
+        }
+    
+    private:
+        int posX;
+        int posY;
+}
+
+class Direction {
+    public:
+        Direction() {
+            dirX = 1;
+            dirY = 0;
+        }
+
+        void toUp() {
+            dirX = 0;
+            dirY = 1;
+        }
+
+        void toDown() {
+            dirX = 0;
+            dirY = -1;
+        }
+
+        void toLeft() {
+            dirX = -1;
+            dirY = 0;
+        }
+
+        void toRight() {
+            dirX = 1;
+            dirY = 0;
+        }
+
+    private:
+        short dirX;
+        short dirY;
+}
+
+
+class BodyPart {
+    public:
+        void appendPart();
+    private:
+        GamePos pos;
+        BodyPart nextPart;
+}
+
+class Snake {
+    public:
+        void move() {
+
+        }
+
+        void draw() {
+
+        }
+
+        GamePos getPos();
+
+    private:
+        BodyPart head;
+        Direction currentDirection;
+    
+}
